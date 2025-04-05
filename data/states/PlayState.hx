fa0ad0385d9e2a65cf4250664b362e2c0fb78f4e
Harcvuk <82612524+Harcvuk@users.noreply.github.com>
2025-04-05 02:11:58 -0400

    More placeholders, along with some credits for some stuff

---
+++ data/states/PlayState.hx
@@ -0,0 +1,9 @@
+public var lerpScore:Int = 0;
+public var intendedScore:Int = 0;
+
+function update(elapsed){
+    intendedScore = songScore;
+    lerpScore = Math.floor(FlxMath.lerp(intendedScore, lerpScore, Math.exp(-elapsed * 24)));
+    scoreTxt.text = 'Score: ' +lerpScore;
+
+}
