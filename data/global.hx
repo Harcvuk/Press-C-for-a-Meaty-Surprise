43c42a99b9fcddc6c8deca23238d7e7bfb65360e
Bitto <bittofromdrawa@gmail.com>
2025-05-09 23:23:10 +0100

    skippable intro on second playthrough

--- data/global.hx
+++ data/global.hx
@@ -1,26 +1,26 @@
 import funkin.backend.utils.WindowUtils;
-//dev stuff delete later
-import funkin.backend.scripting.GlobalScript;
-import funkin.backend.assets.ModsFolder;
 import funkin.backend.utils.DiscordUtil as Discord;
 import funkin.menus.BetaWarningState;
 import funkin.menus.credits.CreditsMain;
+//dev stuff delete later
+import funkin.backend.scripting.GlobalScript;
+import funkin.backend.assets.ModsFolder;
 
+FlxG.save.data.seezeeSawIntro ??= false;
 
 static var redirectStates:Map<Class<FlxState>, String> = [
-    BetaWarningState => "SeezeeBashIntro",
-    FreeplayState => "PressCplay",
-    CreditsMain => "Credits",
+	TitleState => "SeezeeBashIntro",
+	FreeplayState => "PressCplay",
+	CreditsMain => "Credits",
 ];
 
 function preStateSwitch() {
-    for (redirectState in redirectStates.keys())
-        if (Std.isOfType(FlxG.game._requestedState, redirectState))
-            FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));
+	for (redirectState in redirectStates.keys())
+		if (Std.isOfType(FlxG.game._requestedState, redirectState))
+			FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));
 
 	// if the player is offline lol
-	if (Discord.user != null)
-		WindowUtils.winTitle = (Discord.user.userId == "457176118110191617") ? "Press C for a Meaty Surprise!! - Happy Birthday Seezee!" : "Press C for a Meaty Surprise!!"; 
+	if (Discord.user != null) WindowUtils.winTitle = (Discord.user.userId == "457176118110191617") ? "Press C for a Meaty Surprise!! - Happy Birthday Seezee!" : "Press C for a Meaty Surprise!!";
 }
 
 //actually reload the global script by pressing ctrl f5 (dev stuff delete later)
--- data/states/SeezeeBashIntro.hx
+++ data/states/SeezeeBashIntro.hx
@@ -1,25 +1,30 @@
 var intro:FlxSprite;
 
 function create() {
-    FlxG.sound.music?.stop();
-    intro = new FlxSprite();
-    intro.frames = Paths.getSparrowAtlas("game/menus/intro/sbb");
-    intro.animation.addByPrefix("nin", "nin", 24, false);
-    // for codename there are FlxAxes.XY, X or Y
-    // but normally i use 0x01 for X, 0x10 for Y and just 0x11 for XY
-    intro.screenCenter(0x11); 
-    add(intro);
-    intro.animation.play("nin");
-    intro.animation.finishCallback = function(name:String) {
-        FlxTween.tween(intro, {"scale.x": 0.1, "scale.y": 0.1, alpha: 0, angle: 120}, 2, {ease: FlxEase.quartOut, onComplete: () -> FlxG.switchState(new TitleState())});
-    };
+	trace("WELCOME");
+	FlxG.sound.music?.stop();
+	intro = new FlxSprite();
+	intro.frames = Paths.getSparrowAtlas("game/menus/intro/sbb");
+	intro.animation.addByPrefix("nin", "nin", 24, false);
+	// for codename there are FlxAxes.XY, X or Y
+	// but normally i use 0x01 for X, 0x10 for Y and just 0x11 for XY
+	intro.screenCenter(0x11);
+	add(intro);
+	intro.animation.play("nin");
+	intro.animation.finishCallback = function(name:String) {
+		FlxTween.tween(intro, {"scale.x": 0.1, "scale.y": 0.1, alpha: 0, angle: 120}, 2, {ease: FlxEase.quartOut, onComplete: () -> {
+			FlxG.save.data.seezeeSawIntro = true;
+			FlxG.switchState(new MainMenuState());
+		}});
+	}
 }
 
 function postUpdate() {
-    switch (intro.animation.frameIndex) {
-        case 1: // 0 plays it twice also adding it in create either delays it or makes it earlier no, i think main menu first cuz we need options credits and shit
-            CoolUtil.playMusic(Paths.sound("sbb_splash"));
-        case 106:
-            FlxG.camera.flash(FlxColor.WHITE, 1);
-    }
+	if (controls.ACCEPT && FlxG.save.data.seezeeSawIntro) FlxG.switchState(new MainMenuState());
+	switch (intro.animation.frameIndex) {
+		case 1: // 0 plays it twice also adding it in create either delays it or makes it earlier no, i think main menu first cuz we need options credits and shit
+			CoolUtil.playMusic(Paths.sound("sbb_splash"));
+		case 106:
+			FlxG.camera.flash(FlxColor.WHITE, 1);
+	}
 }
