df950bcd745a268b14d96a36d469d85d6abbc7b0
Hero <68159082+HeroEyad@users.noreply.github.com>
2025-05-14 14:58:55 +0300

    improved intro

--- data/states/SeezeeBashIntro.hx
+++ data/states/SeezeeBashIntro.hx
@@ -1,10 +1,13 @@
 import flixel.addons.display.FlxBackdrop;
 import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.text.FlxTypeText;
 
 var backdrop:FlxBackdrop;
 var intro:FlxSprite;
 var speed:Float = 150;
 var hah:Bool = false;
+var typeText:FlxTypeText;
+
 
 function create() {
 	fromIntro = true;
@@ -23,19 +26,31 @@ function create() {
 	ngc.frames = Paths.getSparrowAtlas("ngc");
 	ngc.animation.addByPrefix("idle", "idle", 10, true);
 	ngc.alpha = 0;
+	ngc.antialiasing = false;
 	ngc.screenCenter(0x11);
-	ngc.scale.set(1.3, 1.3);
+	ngc.scale.set(2, 2);
 	add(ngc);
+
+	typeText = new FlxTypeText(0, ngc.y - 50, FlxG.width, "Brought to you by", 16);
+    typeText.setFormat(Paths.font("sonic2.ttf"), 64, FlxColor.WHITE, "center");
+    typeText.screenCenter(0x11);
+	typeText.y -= 100;
+    add(typeText);
 	intro.animation.play("nin");
 	intro.animation.finishCallback = function(name:String) {
 		backdrop.acceleration.set(500,500);
 		FlxTween.tween(backdrop,{alpha:0},2);
 		ngc.animation.play("idle");
-		FlxTween.tween(intro, {"scale.x": 0.1, "scale.y": 0.1, alpha: 0, angle: 120}, 2, {ease: FlxEase.quartOut}).then(FlxTween.tween(ngc, {alpha: 1}, 2, {ease: FlxEase.quartOut})).then(FlxTween.tween(ngc, {alpha: 0}, 2, {startDelay: 2, ease: FlxEase.quartOut, onComplete: () -> {
-			FlxG.save.data.seezeeSawIntro = true;
-			FlxG.switchState(new MainMenuState());
-		}}));
-	}
+		FlxTween.tween(intro, {"scale.x": 0.1, "scale.y": 0.1, alpha: 0, angle: 120}, 2, {ease: FlxEase.quartOut, onComplete: () -> {
+			typeText.start();
+			FlxTween.tween(ngc, {alpha: 1}, 1, {ease: FlxEase.quartOut, startDelay: 1}).then(FlxTween.tween(typeText, {alpha: 0}, 1, {ease: FlxEase.quartOut, startDelay: 1})).then(FlxTween.tween(ngc, {alpha: 0}, 1, {ease: FlxEase.quartOut, startDelay: 0.5, onComplete: () -> {
+				FlxG.save.data.seezeeSawIntro = true;
+				FlxG.switchState(new MainMenuState());
+			}}));
+			
+		}});
+	
+}
 }
 
 function postUpdate() {
