3bd440b90e17a5f91bba064d43455cb52b9a31f7
Bitto <bittofromdrawa@gmail.com>
2025-05-14 13:56:29 +0100

    no more harc erasure

--- data/states/SeezeeBashIntro.hx
+++ data/states/SeezeeBashIntro.hx
@@ -1,13 +1,7 @@
 import flixel.addons.display.FlxBackdrop;
 import flixel.addons.display.FlxGridOverlay;
 import flixel.addons.text.FlxTypeText;
-
-var backdrop:FlxBackdrop;
-var intro:FlxSprite;
-var speed:Float = 150;
-var hah:Bool = false;
-var typeText:FlxTypeText;
-
+import funkin.backend.MusicBeatState;
 
 function create() {
 	fromIntro = true;
@@ -16,41 +10,51 @@ function create() {
 	add(backdrop);
 
 	FlxG.sound.music?.stop();
-	intro = new FlxSprite();
-	intro.frames = Paths.getSparrowAtlas("game/menus/intro/sbb");
-	intro.animation.addByPrefix("nin", "nin", 24, false);
+	intro = new FunkinSprite(0,0,Paths.image("game/menus/intro/sbb"));
+	intro.addAnim("nin", "nin", 24, false);
 	intro.screenCenter(0x11);
 	add(intro);
 
-	ngc = new FlxSprite();
-	ngc.frames = Paths.getSparrowAtlas("ngc");
-	ngc.animation.addByPrefix("idle", "idle", 10, true);
+	ngc = new FunkinSprite(0,0,Paths.image("ngc"));
+	ngc.addAnim("idle", "idle", 10, true);
 	ngc.alpha = 0;
 	ngc.antialiasing = false;
-	ngc.screenCenter(0x11);
-	ngc.scale.set(2, 2);
+	ngc.screenCenter();
+	ngc.scale.set(2.5,2.5);
+	ngc.x -= 320;
+	ngc.y += 50;
 	add(ngc);
 
-	typeText = new FlxTypeText(0, ngc.y - 50, FlxG.width, "Brought to you by", 16);
-    typeText.setFormat(Paths.font("sonic2.ttf"), 64, FlxColor.WHITE, "center");
-    typeText.screenCenter(0x11);
-	typeText.y -= 100;
-    add(typeText);
-	intro.animation.play("nin");
-	intro.animation.finishCallback = function(name:String) {
-		backdrop.acceleration.set(500,500);
-		FlxTween.tween(backdrop,{alpha:0},2);
-		ngc.animation.play("idle");
+	harc = new FunkinSprite(0,0,Paths.image("harc"));
+	harc.alpha = 0;
+	harc.antialiasing = false;
+	harc.screenCenter();
+	add(harc);
+	harc.x += 280;
+	harc.y += 50;
+
+	broughtBy = new FlxTypeText(0, ngc.y - 50, FlxG.width, "Brought to you by", 16);
+	broughtBy.setFormat(Paths.font("sonic2.ttf"), 64, FlxColor.WHITE, "center");
+	broughtBy.screenCenter(0x11);
+	broughtBy.y -= 100;
+	add(broughtBy);
+
+	intro.playAnim("nin");
+	intro.animation.finishCallback = () -> {
+		FlxTween.tween(backdrop,{alpha:0.25},2);
+		FlxTween.tween(backdrop,{"velocity.x":200,"velocity.y":200},2,{ease:FlxEase.sineInOut});
+		ngc.playAnim("idle");
+		new FlxTimer().start(1,() -> broughtBy.start());
 		FlxTween.tween(intro, {"scale.x": 0.1, "scale.y": 0.1, alpha: 0, angle: 120}, 2, {ease: FlxEase.quartOut, onComplete: () -> {
-			typeText.start();
-			FlxTween.tween(ngc, {alpha: 1}, 1, {ease: FlxEase.quartOut, startDelay: 1}).then(FlxTween.tween(typeText, {alpha: 0}, 1, {ease: FlxEase.quartOut, startDelay: 1})).then(FlxTween.tween(ngc, {alpha: 0}, 1, {ease: FlxEase.quartOut, startDelay: 0.5, onComplete: () -> {
+			for (i in [ngc,harc]) FlxTween.tween(i, {alpha: 1}, 1, {ease: FlxEase.quartOut, startDelay: 1}).then(FlxTween.tween(i, {alpha: 0}, 1, {ease: FlxEase.quartOut,startDelay:1}));
+			FlxTween.tween(backdrop, {alpha: 0}, 1, {ease: FlxEase.quartOut, startDelay: 3});
+			FlxTween.tween(broughtBy, {alpha: 0}, 1, {ease: FlxEase.quartOut, startDelay: 3, onComplete: () -> {
 				FlxG.save.data.seezeeSawIntro = true;
+				MusicBeatState.skipTransOut = true;
 				FlxG.switchState(new MainMenuState());
-			}}));
-			
+			}});
 		}});
-	
-}
+	}
 }
 
 function postUpdate() {
