9d5ebf4eb25f0267454ad7cdb187da17ecbb2fe4
Bitto <bittofromdrawa@gmail.com>
2025-05-15 19:55:04 +0100

    improved Credits.hx

--- data/states/Credits.hx
+++ data/states/Credits.hx
@@ -2,32 +2,20 @@ import flixel.text.FlxTextBorderStyle as Border;
 import flixel.addons.effects.FlxTrail as Trail;
 import EReg;
 
-var names = [
-	"HeroEyad: Director and Programmer of Press C for headache",
-	"TechyHarcvuk: Director and Made everything for Meaty Surprise",
-	"Bitto: Programmer and Charter, Made \"headache-ok\"",
-	"mariofy: 3D-Modeler and Animator",
-	"Gumbalino: 3D-Modeler and Animator",
-	"Hipix: Artist and Animator",
-	"douyhe: Composer",
-	"Verwex: Birthday Bash invitation",
-	"Terionic: Code assistance",
-	"Happy Birthday Seezee!"
+var credits = [
+	{text:"HeroEyad: Director and Programmer of Press C for headache",color:0xFF00FFFF},
+	{text:"TechyHarcvuk: Director and Made everything for Meaty Surprise",color:0xFFFF8000},
+	{text:"Bitto: Programmer and Charter, Made \"headache-ok\"",color:0xFFFF0000},
+	{text:"mariofy: 3D-Modeler and Animator",color:0xFF33BB33},
+	{text:"Gumbalino: 3D-Modeler and Animator",color:0xFFAA33DD},
+	{text:"Hipix: Artist and Animator",color:0xFFFFCC33},
+	{text:"douyhe: Composer",color:0xFF0088FF},
+	{text:"Verwex: Birthday Bash invitation",color:0xFF9933EE},
+	{text:"Terionic: Code assistance",color:0xFF66EE44},
+	{text:"Happy Birthday Seezee!"}
 ];
 
-var colors = [
-	0xFF00FFFF,
-	0xFFFF8000,
-	0xFFFF0000,
-	0xFF33BB33,
-	0xFFAA33DD,
-	0xFFFFCC33,
-	0xFF0088FF,
-	0xFF9933EE,
-	0xFF66EE44
-];
-
-var credits = [];
+var creditTextSprites = [];
 function create() {
 	FlxG.sound.music.volume = 1;
 	camCredits = new FlxCamera();
@@ -42,18 +30,18 @@ function create() {
 	add(logo);
 
 	var spacing = 40;
-	for (i in 0...names.length) {
-		var text = new FlxText(0, (FlxG.height + 100) + i * spacing, FlxG.width, names[i]).setFormat(Paths.font("sonic2.ttf"), 32, colors[i] ?? 0xFFFFFF00, "center",Border.SHADOW,0xFF000000).screenCenter(0x01); // center horizontally
+	for (i in 0...credits.length) {
+		var text = new FlxText(0, (FlxG.height + 100) + i * spacing, FlxG.width, credits[i].text).setFormat(Paths.font("sonic2.ttf"), 32, credits[i].color ?? 0xFFFFFF00, "center",Border.SHADOW,0xFF000000).screenCenter(0x01); // center horizontally
 		text.borderSize *= 2;
 		text.y = FlxG.height + 50 + i * spacing;
 		add(text);
-		credits.push(text);
+		creditTextSprites.push(text);
 
-		FlxTween.tween(text, {y: (FlxG.height / 2 - (names.length * spacing) / 2 + i * spacing)+110}, 1.5, {startDelay: i * 0.15, ease: FlxEase.expoOut});
+		FlxTween.tween(text, {y: (FlxG.height / 2 - (credits.length * spacing) / 2 + i * spacing)+110}, 1.5, {startDelay: i * 0.15, ease: FlxEase.expoOut});
 	}
-	credits[credits.length-1].scale.set(2,2);
-	credits[credits.length-1].updateHitbox();
-	credits[credits.length-1].screenCenter(0x01);
+	creditTextSprites[credits.length-1].scale.set(2,2);
+	creditTextSprites[credits.length-1].updateHitbox();
+	creditTextSprites[credits.length-1].screenCenter(0x01);
 }
 
 var timings = 0.25;
@@ -81,13 +69,13 @@ function update(elapsed) {
 		FlxTween.tween(letter,{y:FlxG.height*3},FlxG.random.int(6,30),{onComplete: () -> {
 			for (i in [trail,letter]) {
 				remove(i);
-				members.remove(i);
 				i.destroy();
 			}
 		}});
 	}
 
 	if (FlxG.keys.justPressed.C && canC) {
+		logo.loadSprite(Paths.image("logoC94"));
 		canC = false;
 		var cFlash = new FunkinSprite(0,0,Paths.image("C"));
 		cFlash.camera = camCredits;
@@ -96,9 +84,8 @@ function update(elapsed) {
 		FlxTween.tween(cFlash,{alpha:0},1);
 		FlxG.sound.play(Paths.sound("C"));
 		FlxG.sound.music.volume = 0;
-		new FlxTimer().start(4,() -> FlxG.sound.music.fadeOut(2,1));
 
-		for (credit in credits) {
+		for (credit in creditTextSprites) {
 			credit.text = new EReg("[A-Z]","g").replace(credit.text,"C");
 			credit.text = new EReg("[a-z]","g").replace(credit.text,"c");
 		}
@@ -116,16 +103,15 @@ function update(elapsed) {
 		var trail = new Trail(letter, null, 20, 10, 0.4, 0.05);
 		insert(0,trail);
 
-		FlxTween.tween(letter,{y:FlxG.height},10,{onComplete: () -> {
-			for (i in [trail,letter]) {
-				remove(i);
-				members.remove(i);
-				i.destroy();
-			}
-			for (credit in credits) {
-				credit.text = new EReg("[A-Z]","g").replace(credit.text,"C");
-				credit.text = new EReg("[a-z]","g").replace(credit.text,"c");
-			}
+		FlxTween.tween(letter,{y:FlxG.height},10,{onComplete: () -> for (i in [trail,letter]) {
+			remove(i);
+			i.destroy();
 		}});
+
+		var si = FlxG.sound.load(Paths.sound("si si si"));
+		si.play();
+		si.pitch = 1/4;
+		FlxTween.tween(si,{pitch:4},15);
+		si.onComplete = () -> FlxG.sound.music.fadeOut(2,1);
 	}
 }
