4d207f10e17dcc68b68274fb1f446ecd20826f8f
Bitto <bittofromdrawa@gmail.com>
2025-05-18 20:11:13 +0100

    la can do flips now also botplay friendly

--- songs/tense/scripts/la movements.hx
+++ songs/tense/scripts/la movements.hx
@@ -1,22 +1,28 @@
 laX = dad.x;
 laY = dad.y;
 var la = dad;
-function postCreate() {
-	la.moves = true;
-}
-function onDadHit(e) {
+function postCreate() la.moves = true;
+
+function onNoteHit(e) {
+	if (e.character.curCharacter != "la") return;
 	if (!e.isSustainNote) la.x = laX;
 	switch (e.direction) {
 		case 0: la.velocity.x = -20;
 		case 1: if (la.y != laY) {
 			FlxTween.cancelTweensOf(la);
 			FlxTween.tween(la,{y:laY},0.1);
+			la.angle = 0;
 		}
 		case 2:
 			if (la.y != laY) return;
-			FlxTween.cancelTweensOf(la);
+			FlxTween.cancelTweensOf(la,["y"]);
 			la.y = laY;
 			FlxTween.tween(la,{y:laY - 20},Conductor.crochet/2000,{ease:FlxEase.sineOut,onComplete:() -> if (la.animation.curAnim.name == "singUP") la.playAnim("fall",true,false,0,"LOCK")}).then(FlxTween.tween(la,{y:laY},Conductor.crochet/2000,{ease:FlxEase.sineIn, onComplete:() -> if (la.animation.curAnim.name == "fall") la.playAnim("idle",true,false,0,"DANCE")}));
+			if (FlxG.random.bool(5)) {
+				FlxTween.cancelTweensOf(la,["angle"]);
+				la.angle = 0;
+				FlxTween.tween(la,{angle:360*FlxG.random.sign()},Conductor.crochet/1000,{onComplete: () -> la.angle = 0});
+			}
 	}
 }
 
