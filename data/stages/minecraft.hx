c862357f0108025528054aa03e10f80aa9457a10
Bitto <bittofromdrawa@gmail.com>
2025-05-19 18:08:07 +0100

    on mobile cant check if works

--- data/stages/minecraft.hx
+++ data/stages/minecraft.hx
@@ -1,4 +1,4 @@
 function password() {
-	for (thing in [sign,hand]) FlxTween.tween(thing,{x:thing.x-120},Conductor.crochet/1000,{ease:FlxEase.circOut});
-	FlxTween.tween(hand,{x:hand.x+120},Conductor.crochet/1000,{ease:FlxEase.circIn,startDelay:Conductor.crochet/1000});
+	for (thing in [sign,hand]) FlxTween.tween(thing,{x:thing.x-120},Conductor.crochet/1000,{ease:FlxEase.circOut}).then(
+	FlxTween.tween(hand,{x:hand.x+120},Conductor.crochet/1000,{ease:FlxEase.circIn}));
 }
