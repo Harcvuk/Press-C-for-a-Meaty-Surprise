466129dc775e19da69c8e255b80c866d52284f3d
Harcvuk <82612524+Harcvuk@users.noreply.github.com>
2025-05-19 16:43:15 -0400

    Jagger can now die lmfao

--- data/states/GameOverSubstate.hx
+++ data/states/GameOverSubstate.hx
@@ -4,5 +4,9 @@ function create() {
             gameOverSong = 'gameOverPeak'; // Yes I coded this myself, used this for my april fools desire mod. For some reason github is fucking stupid and it removed this file initially
             lossSFXName = "PeakGameOverSFX";
             retrySFX = "PeakgameOverEnd"; // This however is something I didn't use for that mod. But it's a very easy thing to add, so this isn't too big of a deal tbh lmao
+        case 'too-peak':
+            gameOverSong = 'gameOverTweak';
+            lossSFXName = "TweakGameOverSFX";
+            retrySFX = "TweakgameOverEnd";
     }
 }
