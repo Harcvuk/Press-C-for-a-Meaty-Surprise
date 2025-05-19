cfab1e628700fad610b0c56726a77d4010add915
Harcvuk <82612524+Harcvuk@users.noreply.github.com>
2025-05-19 16:15:28 -0400

    Github, don't delete files without my fucking permission

---
+++ data/states/GameOverSubstate.hx
@@ -0,0 +1,8 @@
+function create() {
+    switch (PlayState.SONG.meta.name) {
+        case 'peakingtrial':
+            gameOverSong = 'gameOverPeak'; // Yes I coded this myself, used this for my april fools desire mod. For some reason github is fucking stupid and it removed this file initially
+            lossSFXName = "PeakGameOverSFX";
+            retrySFX = "PeakgameOverEnd"; // This however is something I didn't use for that mod. But it's a very easy thing to add, so this isn't too big of a deal tbh lmao
+    }
+}
