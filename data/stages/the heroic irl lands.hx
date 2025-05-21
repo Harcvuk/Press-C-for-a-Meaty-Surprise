5fef296b35d6e9e3af3c2ac743401af606aa6f7e
Hero <eyadelmokadem123456789@gmail.com>
2025-05-22 00:51:22 +0300

    hmm

--- data/stages/the heroic irl lands.hx
+++ data/stages/the heroic irl lands.hx
@@ -1,10 +1,8 @@
 function plop() puddle.playAnim("plop",true);
 
 function superPlop() {
-	boyfriend.alpha = 0;
-	gf.alpha = 0;
+	boyfriend.alpha = gf.alpha = 0;
 	plop();
 	camGame.scroll.set(970,870);
-	camGame.followLerp = 0;
-	camHUD.alpha = 0;
+	camGame.followLerp =	camHUD.alpha = 0;
 }
