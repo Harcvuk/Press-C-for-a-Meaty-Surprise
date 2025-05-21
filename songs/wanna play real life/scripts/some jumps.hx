3eea6c8edba4dcba02ea77065e6b5a7011a84899
Bitto <bittofromdrawa@gmail.com>
2025-05-21 10:51:32 +0100

    jump sounds also too peaked chart fix

--- songs/wanna play real life/scripts/some jumps.hx
+++ songs/wanna play real life/scripts/some jumps.hx
@@ -1,16 +1,16 @@
 var la = boyfriend;
 var laY = la.y;
-
+var jumpSpeed = 1500;
 function update() {
-    if (FlxG.keys.pressed.C) {
-        if (la.y != laY) return;
-			FlxTween.cancelTweensOf(la,["y"]);
-			la.y = laY;
-			FlxTween.tween(la,{y:laY - 300},Conductor.crochet/1200,{ease:FlxEase.sineOut,onComplete:() -> if (la.animation.curAnim.name == "singUP") la.playAnim("fall",true,false,0,"LOCK")}).then(FlxTween.tween(la,{y:laY},Conductor.crochet/1200,{ease:FlxEase.sineIn, onComplete:() -> if (la.animation.curAnim.name == "fall") la.playAnim("idle",true,false,0,"DANCE")}));
-			if (FlxG.random.bool(50)) {
-				FlxTween.cancelTweensOf(la,["angle"]);
-				la.angle = 0;
-				FlxTween.tween(la,{angle:360*FlxG.random.sign()},Conductor.crochet/600,{onComplete: () -> la.angle = 0});
-            }
-    }
+	if (FlxG.keys.pressed.C && la.y == laY) {
+		FlxTween.cancelTweensOf(la,["y"]);
+		la.y = laY;
+		FlxTween.tween(la,{y:laY - 300},Conductor.crochet/jumpSpeed,{ease:FlxEase.sineOut}).then(FlxTween.tween(la,{y:laY},Conductor.crochet/jumpSpeed,{ease:FlxEase.sineIn}));
+		if (FlxG.random.bool(25)) {
+			FlxG.sound.play(Paths.sound("marJump"));
+			FlxTween.cancelTweensOf(la,["angle"]);
+			la.angle = 0;
+			FlxTween.tween(la,{angle:360*FlxG.random.sign()},Conductor.crochet/(jumpSpeed/2),{onComplete: () -> la.angle = 0});
+		} else FlxG.sound.play(Paths.sound("marJumpy"));
+	}
 }
