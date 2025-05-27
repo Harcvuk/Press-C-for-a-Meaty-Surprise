57ad5c6a26d81cdf4acd0278adfc177ea5ab0db4
mario <97336404+mamrofy@users.noreply.github.com>
2025-05-27 14:47:22 +0100

    wip (by :EYAD: on parsec)
    Co-Authored-By: Hero <hero@heroeyad.xyz>

--- data/states/mcpause.hx
+++ data/states/mcpause.hx
@@ -3,6 +3,11 @@ import flixel.FlxSprite;
 camPause = new FlxCamera();
 var open = FlxG.sound.load(Paths.sound("mcpause/select"));
 
+boxes = [];
+text = [];
+
+var curSelected:Int = 0;
+
 function create(e) {
     cameras = [camPause];
     e.cancel();
@@ -10,23 +15,31 @@ function create(e) {
     camPause.bgColor = 0x80000000;
 	FlxG.cameras.add(camPause,false);
 
-    temp=new FunkinSprite(0,0,Paths.image("hud/mcpause/temp"));
-    temp.alpha=0.5;
-    add(temp);
+    add(temp=new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK)).alpha = 0.5;
 
-    playButton = new FlxSprite(435,195);
-    playButton.loadGraphic(Paths.image('hud/mcpause/button_unhighlited'));
-    playButton.setGraphicSize(Std.int(playButton.width * 2.05));
-    playButton.updateHitbox();
-    add(playButton);
+    for (s => ev in menuItems) {
+        var offset = 195+(s * 50);
+        var box:FlxSprite = new FlxSprite(500, offset).loadGraphic(Paths.image('hud/mcpause/button_unhighlited'));
+        box.setGraphicSize(Std.int(box.width * 2.05));
+        box.screenCenter(0x01);
+        boxes.push(box);
+        add(box);
+    
+        var options = ["Resume", "Restart", "Change Controls", "Options", "Exit"]; // for now
+        var nin:FlxText = new FlxText(500, offset-5, 0, options[s], 16).setFormat(Paths.font("minecraft.ttf"), 24, FlxColor.WHITE, "center");
+        nin.screenCenter(0x01);
+        add(nin);
+        text.push(nin);
 
-    playButton = new FlxSprite(435,387);
-    playButton.loadGraphic(Paths.image('hud/mcpause/button_unhighlited'));
-    playButton.setGraphicSize(Std.int(playButton.width * 2.05));
-    playButton.updateHitbox();
-    add(playButton);
+    }
 }
 
 function update() {
-    if (controls.ACCEPT) close();
+    if (controls.ACCEPT) selectOption();
+    if (controls.UP_P || controls.DOWN_P) changeSelection(controls.UP_P ? -1 : 1);
+}   
+
+function changeSelection(c) {
+    curSelected = FlxMath.wrap(curSelected + c, 0, menuItems.length - 1);
+    trace(curSelected);
 }
