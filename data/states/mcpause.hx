25d550de2e51a8f15f97bf366b29697820565f23
mario <97336404+mamrofy@users.noreply.github.com>
2025-05-27 16:55:38 +0100

    yu need to put in the menu sound ok

--- data/states/mcpause.hx
+++ data/states/mcpause.hx
@@ -1,45 +1,59 @@
-import flixel.FlxSprite;
-
-camPause = new FlxCamera();
+import funkin.backend.Conductor;
+import funkin.menus.PauseSubState;
+import funkin.game.PlayState;
+import flixel.text.FlxTextBorderStyle;
 var open = FlxG.sound.load(Paths.sound("mcpause/select"));
 
-boxes = [];
-text = [];
-
-var curSelected:Int = 0;
-
 function create(e) {
-    cameras = [camPause];
-    e.cancel();
-
-    camPause.bgColor = 0x80000000;
-	FlxG.cameras.add(camPause,false);
-
-    add(temp=new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK)).alpha = 0.5;
-
-    for (s => ev in menuItems) {
-        var offset = 195+(s * 50);
+	boxes = new FlxGroup();
+	grpText = new FlxGroup();
+	textCam = new FlxCamera();
+    textCam.bgColor = 0x80000000;
+    
+	// add(temp=new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK)).alpha = 0.5;
+	var i = 0;
+    for(e in menuItems) {
+        var offset = 252+(i * 50);
         var box:FlxSprite = new FlxSprite(500, offset).loadGraphic(Paths.image('hud/mcpause/button_unhighlited'));
         box.setGraphicSize(Std.int(box.width * 2.05));
         box.screenCenter(0x01);
-        boxes.push(box);
+		box.camera = textCam;
+        boxes.add(box);
         add(box);
     
-        var options = ["Resume", "Restart", "Change Controls", "Options", "Exit"]; // for now
-        var nin:FlxText = new FlxText(500, offset-5, 0, options[s], 16).setFormat(Paths.font("minecraft.ttf"), 24, FlxColor.WHITE, "center");
-        nin.screenCenter(0x01);
-        add(nin);
-        text.push(nin);
-
-    }
+        var nin:FlxText = new FlxText(500, offset-4, 0, e, 16).setFormat(Paths.font("minecraft.ttf"), 20, FlxColor.WHITE, "center", FlxTextBorderStyle.SHADOW, FlxColor.BLACK);
+		nin.borderSize = 2;
+        nin.borderColor = 0x99000000;
+		nin.screenCenter(0x01);
+		nin.camera = textCam;
+        grpText.add(nin);
+		i++;
+	}
+	
+	add(grpText);
 }
 
-function update() {
-    if (controls.ACCEPT) selectOption();
-    if (controls.UP_P || controls.DOWN_P) changeSelection(controls.UP_P ? -1 : 1);
-}   
+function onChangeItem(){
+	var ok = 'hud/mcpause/button_';
+	FlxG.sound.play(open);
+	new FlxTimer().start(0.01, ()->{
+		for(e in boxes.members)
+			e.loadGraphic(Paths.image(ok+'unhighlited'));
 
-function changeSelection(c) {
-    curSelected = FlxMath.wrap(curSelected + c, 0, menuItems.length - 1);
-    trace(curSelected);
+		boxes.members[curSelected].loadGraphic(Paths.image(ok+'highlighted'));
+	});
+}
+
+function destroy()
+	FlxG.cameras.remove(textCam, false);
+
+function postCreate()
+{
+	FlxG.cameras.add(textCam, false);
+	FlxTween.num(0, 0.5, 5, {}, function(num) {pauseMusic.volume = num;});
+	for (i=>item in grpMenuShit.members)
+		item.visible = false;
+		
+	for (label in [levelInfo, levelDifficulty, deathCounter, multiplayerText])
+		label.visible = false;
 }
